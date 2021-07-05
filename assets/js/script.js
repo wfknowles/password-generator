4cc2b7d7a0a5535accf0fdee2fc46a4526d14f8a
Will Knowles <will@willknowles.com>
2021-07-05 15:32:21 -0500

    Initial structure of functionality created

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,65 @@
 // Assignment code here
 
+/* 
+  On click 
+    - generate prompt for password length (Value between 8 and 128)
+    - generate a series of prompts for lowercase, uppercase, numeric, and/or special characters
+    - validate responses with minimum criteria being at least one selected character type
+    - create password per requirements
+    - send to ui
+*/
+
+function validateLength(len) {
+
+}
+
+function validateCharacters(chars) {
+
+}
+
+function validateRequirements(reqs) {
+  var len = idateLength(reqs.length);
+  var chars = validateCharacters(reqs.characters);
+}
+
+function createPassword(reqs) {
+
+}
+
+function generateRequirements() {
+
+  //Gather Responses
+  var length = window.prompt("How many characters do you want in your password? Input a value between 8 and 128.");
+  var lowercase = window.confirm("Do you want to use lowercase letters in your new password?");
+  var uppercase = window.confirm("Do you want to use uppercase letters in your new password?");
+  var numeric = window.confirm("Do you want to use numbers in your new password?");
+  var special = window.confirm("Do you want to use special characters in your new password?");
+
+  //Formate Responses As Object
+  var reqs = {
+    length: Number(length),
+    characters: {
+      lowercase: lowercase,
+      uppercase: uppercase,
+      numeric: numeric,
+      special: special
+    }
+  }
+
+  console.log(reqs);
+  // if (validateRequirements(reqs)) {
+  //   return reqs;
+  // } else {
+  //   return false;
+  // }
+}
+
+function generatePassword() {
+  var requirements = generateRequirements();
+  // var generatedPassword = createPassword(requirements);
+  // return generatedPassword;
+}
+
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
